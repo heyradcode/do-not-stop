3e8b31cbfc3051c7c0d1fd5b42114b3371d9ce35
aichannode <aichannode@gmail.com>
2026-05-25 22:56:27 -0400

    chore: remove unused route exports

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,5 +1,1 @@
 export { default as AppRoutes } from '@router/app-routes';
-export { default as BattleRoute } from '@router/routes/battle-route';
-export { default as BreedRoute } from '@router/routes/breed-route';
-export { default as LevelUpRoute } from '@router/routes/level-up-route';
-export { default as RenameRoute } from '@router/routes/rename-route';
