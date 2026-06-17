b752c25e92c52cec6220a6b843fe6bd0127b8aab
cdsaidev <cdsgmacct@gmail.com>
2026-06-17 15:13:01 -0400

    feat: add useSolanaFees hook reading GlobalState + PlayerProfile PDAs

--- shared/src/hooks/chains/solana/index.ts
+++ shared/src/hooks/chains/solana/index.ts
@@ -1,3 +1,4 @@
 export { useProgram, type SolanaProgram } from './useProgram';
 export { usePets, type PetRow } from './usePets';
 export { usePetActions } from './usePetActions';
+export { useSolanaFees, type SolanaFees } from './useSolanaFees';
